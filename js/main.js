57627209e48ddf62ab1fe869cbdbcf28e6b7d532
mildmocha <128768439+mildmocha@users.noreply.github.com>
2023-06-29 19:53:39 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }, 200);
 });
+const modal = document.querySelector('.modal')
+const modal2 = document.querySelector('.modal2')
+const modal3 = document.querySelector('.modal3')
+
+const open = document.querySelector('.profileBtn')
+const open2 = document.querySelector('.projectBtn')
+const open3 = document.querySelector('.contactBtn')
+
+const closeBtns = document.querySelectorAll('.closeBtn');
+
+
+closeBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('open');
+    modal2.classList.remove('open');
+    modal3.classList.remove('open');
+  });
+});
+open.addEventListener('click', () =>{
+  modal.classList.add('open')
+})
+open2.addEventListener('click', () =>{
+  modal2.classList.add('open')
+})
+open3.addEventListener('click', () =>{
+  modal3.classList.add('open')
+})
+
+
