53bccc728a8428861e26c43cb2df390ff2f6e959
mildmocha <128768439+mildmocha@users.noreply.github.com>
2023-07-18 13:39:05 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const modal2 = document.querySelector(".modal2");
 const modal3 = document.querySelector(".modal3");
 
 const projectsFlip = document.querySelectorAll(".projectC");
-const contactsFlip =document. querySelectorAll(".contactWrap");
+const contactsFlip = document.querySelectorAll(".contactWrap");
 
 const open = document.querySelector(".profileBtn");
 const open2 = document.querySelector(".projectBtn");
@@ -53,15 +53,17 @@ closeBtns.forEach((btn) => {
     modal.classList.add("out");
 
     modal2.classList.add("out");
- projectsFlip.forEach((abc)=>{
-  abc.classList.remove("flip");
- })
+    setTimeout(() => {
+      projectsFlip.forEach((abc) => {
+        abc.classList.remove("flip");
+      })
+    }, 400)
 
     modal3.classList.add("out");
-    contactsFlip.forEach((abc)=>{
+    contactsFlip.forEach((abc) => {
       abc.classList.remove("flip");
-     })
-    
+    })
+
   });
 });
 
@@ -75,14 +77,17 @@ open.addEventListener("click", () => {
 open2.addEventListener("click", () => {
   modal2.classList.add("open");
   modal2.classList.remove("out");
-setTimeout(()=>{
-  projectsFlip.forEach((abc, index) => {
-    setTimeout(() => {
-      abc.classList.add("flip");
-    }, index * 300)
-     // 딜레이를 적용할 시간 (밀리초 단위로 설정)
-  });
-},300);
+  contactsFlip.forEach((abc) => {
+    abc.classList.remove("flip")
+  })
+  setTimeout(() => {
+    projectsFlip.forEach((abc, index) => {
+      setTimeout(() => {
+        abc.classList.add("flip");
+      }, index * 300)
+      // 딜레이를 적용할 시간 (밀리초 단위로 설정)
+    });
+  }, 400);
 });
 //  projects.forEach((project)=>{
 //  const projectWidth = project.offsetWidth;
@@ -94,13 +99,14 @@ open3.addEventListener("click", () => {
   console.log('1')
   modal3.classList.add("open");
   modal3.classList.remove("out");
-setTimeout(()=>{
-  contactsFlip.forEach((abc) => {
-    
+
+  setTimeout(() => {
+    contactsFlip.forEach((abc) => {
+
       abc.classList.add("flip");
-    } )
-     // 딜레이를 적용할 시간 (밀리초 단위로 설정)
-  },400);
+    })
+    // 딜레이를 적용할 시간 (밀리초 단위로 설정)
+  }, 400);
 });
 document.addEventListener("DOMContentLoaded", () => {
   const projects = document.querySelectorAll(".projects");
@@ -125,6 +131,8 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", () => {
       modalContent.forEach((projectModal) => {
         projectModal.classList.add("out");
+
+
       });
     });
   });
